2bcc357ae1ec5ea177e3dc1ac7d4eddb91f1abda
Artem <o_artem@bk.ru>
2023-02-11 15:44:32 +0300

    задача 4

---
+++ задача 1/задача 2/задача 3/задача 4/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 1;
+ int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+ a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + a);
+while (i <= a)
+ {
+    if (i % 2 != 1)
+        {
+            Console.Write(i + ", ");
+             not = false;
+}
+i++;
+}
+
+if (not)
+ {
+    Console.WriteLine("Нет чётных чисел!");
+ }
